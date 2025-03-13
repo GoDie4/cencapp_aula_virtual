27b32b85231017babcdd82705faf77947f38cfee
RakonaDev <juancajas1905@gmail.com>
2025-03-13 09:14:19 -0500

    d

--- aula_virtual/app/(dashboard)/aula/materiales/page.tsx
+++ aula_virtual/app/(dashboard)/aula/materiales/page.tsx
@@ -2,10 +2,21 @@ import { NextPage } from "next";
 import Materiales from "./@components/Materiales";
 import { TitleAula } from "../../@components/estructura/TitleAula";
 
-const Page: NextPage = () => {
+export async function getStaticProps() {
+  const res = await fetch('https://api.example.com/data');
+  const data :  = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
+const Page: NextPage = async ({ data }) => {
   return (
     <div>
-      <TitleAula titulo="Materiales por Curso"/>
+      <TitleAula titulo="Materiales por Curso" />
       <Materiales />
     </div>
   );
