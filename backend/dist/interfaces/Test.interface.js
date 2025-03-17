1ce3b55f9793474690da7ffb183cd6fd88f8c3cc
GoDie4 <anthony10.reyes10@gmail.com>
2025-03-17 16:05:07 -0500

    Cambiando cookie

--- backend/dist/interfaces/Test.interface.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var TipoPrueba;
-(function (TipoPrueba) {
-    TipoPrueba["EXAMEN"] = "EXAMEN";
-    TipoPrueba["EJERCICIOS"] = "EJERCICIOS";
-})(TipoPrueba || (TipoPrueba = {}));
-//# sourceMappingURL=Test.interface.js.map
